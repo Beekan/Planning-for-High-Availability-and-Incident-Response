0b6c03c15924a9b2e446707e47ab4ef7e9d48165
Beeko <s-mohamed.hamed@zewailcity.edu.eg>
2023-02-26 03:29:54 +0200

    Final Changes

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -1,4 +1,4 @@
-
+variable primary_db_cluster_arn {}
 
 resource "aws_rds_cluster_parameter_group" "cluster_pg-s" {
   name   = "udacity-pg-s"
@@ -34,7 +34,7 @@ resource "aws_rds_cluster" "udacity_cluster-s" {
   storage_encrypted        = false
   replication_source_identifier = var.primary_db_cluster_arn
   source_region            = "us-east-2"
-  depends_on = [aws_rds_cluster_parameter_group.cluster_pg-s]
+  depends_on = [var.primary_db_instance_arn, aws_rds_cluster_parameter_group.cluster_pg-s]
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance-s" {
